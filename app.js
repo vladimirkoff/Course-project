0fa7e2a08a29ccc07675a985b3719f78098aab44
Vladimir <vovakovalev2016@icloud.com>
2023-05-28 16:29:19 +0300

    minor changes

--- app.js
+++ app.js
@@ -10,10 +10,8 @@ function btn() {
     var dice1 = Math.floor(Math.random() * 6) + 1;
     var dice2 = Math.floor(Math.random() * 6) + 1;
 
-    //2. display result: unhiding the dice after 'rolling' it
     document.getElementById('dice-1').style.display = 'block';
     document.getElementById('dice-2').style.display = 'block';
-    // changing the dice image src
     document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
     document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
